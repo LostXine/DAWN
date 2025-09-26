9be498d66983de1646228f09f697ffca434cf5e6
E Ro Nguyen <eronguyen@E-Ros-MacBook-Pro.local>
2025-09-26 12:52:50 -0400

    Init page

--- dawn/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_dataset import BaseDataset
-from .calvin.calvin import CalvinDataset
-from .droid.droid import DroidDataset
--- results.py
+++
@@ -1,9 +0,0 @@
-import json
-import glob
-path = "/home/nero/Robotics/DAWN/outputs/DAWN_infer"
-
-lst = sorted(glob.glob(f"{path}/*/results.json"))
-
-for file in lst:
-    data = json.load(open(file, "r"))
-    print(f"File: {file}: {data['avg_seq_len']}")
